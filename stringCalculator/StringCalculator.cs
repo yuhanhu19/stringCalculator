49dc6141301b5583c79198c85a50931fdaad80d6
Yuhan Hu <yuhan.hu@cat.com>
2022-12-29 14:39:50 +0800

    [Yuhan] feat: add implementation to return sum of two parameters

--- stringCalculator/StringCalculator.cs
+++ stringCalculator/StringCalculator.cs
@@ -6,11 +6,16 @@ namespace stringCalculator
     {
         public int Add(string numAsString)
         {
-            if (String.IsNullOrEmpty(numAsString))
+            if (string.IsNullOrEmpty(numAsString))
             {
                 return 0;
             }
             return 1;
+        }
+        
+        public int Add(string numAsString1, string numAsString2)
+        {
+            return int.Parse(numAsString1)+int.Parse(numAsString2);
         }
     }
 }
