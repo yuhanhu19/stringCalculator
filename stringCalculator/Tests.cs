2df7de46abb10428a2d24bf3252f1908c9b524fa
Yuhan Hu <yuhan.hu@cat.com>
2022-12-29 14:29:30 +0800

    [Yuhan] test: add test ShouldReturn3GivenStringWith1And2

--- stringCalculator/Tests.cs
+++ stringCalculator/Tests.cs
@@ -20,5 +20,13 @@ namespace stringCalculator
             var result = stringCalculator.Add("1");
             Assert.Equal(1, result);
         }
+        
+        [Fact]
+        public void ShouldReturn3GivenStringWith1And2()
+        {
+            var stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("1", "2");
+            Assert.Equal(3, result);
+        }
     }
 }
