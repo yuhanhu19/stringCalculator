d8431620e1ab6b2a4a43649e336975e070fa16a6
Yuhan Hu <yuhan.hu@cat.com>
2022-12-29 16:34:42 +0800

    [Yuhan] test: add test ShouldThrowExceptionWhenInputEndsWithDelimiter

--- stringCalculator/Tests.cs
+++ stringCalculator/Tests.cs
@@ -20,7 +20,7 @@ namespace stringCalculator
             var result = stringCalculator.Add("1");
             Assert.Equal(1, result);
         }
-        
+
         [Fact]
         public void ShouldReturn3GivenStringWith1And2SeparatedByCommas()
         {
@@ -28,13 +28,21 @@ namespace stringCalculator
             var result = stringCalculator.Add("1,2");
             Assert.Equal(3, result);
         }
-        
+
         [Fact]
         public void ShouldReturn6GivenStringWith1And2And3SeparatedByNewLinesAndCommas()
         {
             var stringCalculator = new StringCalculator();
             var result = stringCalculator.Add("1\n2,3");
             Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenInputEndsWithDelimiter()
+        {
+            var stringCalculator = new StringCalculator();
+            var exception = Assert.Throws<FormatException>(() => stringCalculator.Add("1,2,"));
+            Assert.Equal("Number expected but EOF found.",exception.Message);
         }
     }
 }
