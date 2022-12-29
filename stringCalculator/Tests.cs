5a70a20ca6fe0197ce7bf067a4fb474f56b6abe3
Yuhan Hu <yuhan.hu@cat.com>
2022-12-29 11:47:49 +0800

    [Yuhan] test: add test ShouldReturn0GivenEmptyString

---
+++ stringCalculator/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace stringCalculator
+{
+    public class Tests
+    {
+        [Fact]
+        public void ShouldReturn0GivenEmptyString()
+        {
+            var stringCalculator = new StringCalculator();
+            var result = stringCalculator.Add("");
+            Assert.Equal(0, result);
+        }
+    }
+}
